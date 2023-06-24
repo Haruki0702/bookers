a7a8cb7a9360aa8e115dabcc9a1b87f9bc7c71d4
Haruki0702 <23280696+Haruki0702@users.noreply.github.com>
2023-06-24 06:53:06 +0000

    destroyまで終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,8 @@
 class BooksController < ApplicationController
-  def new
 
-    @book=Book.new
-
-  end
   def index
+    @books=Book.all
+    @book=Book.new
   end
   def create
     book=Book.new(book_params)
@@ -13,13 +11,23 @@ class BooksController < ApplicationController
   end
 
 
+  def edit
+    @book=Book.find(params[:id])
+  end
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def show
     @book=Book.find(params[:id])
   end
-
-  def edit
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
   end
-
   private
   def book_params
     params.require(:book).permit(:title, :body)
